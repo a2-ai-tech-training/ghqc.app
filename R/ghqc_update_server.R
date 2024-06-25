29bbcfd228119580b1266c0e8957b9fe43c93751
Anne Zheng <anne@a2-ai.com>
2024-06-25 20:17:11 +0000

    list files -> dir ls

--- R/ghqc_update_server.R
+++ R/ghqc_update_server.R
@@ -8,7 +8,7 @@ ghqc_update_server <- function(id) {
     ns <- session$ns
 
     observe({
-      issues <- get_issues2()
+      issues <- get_issues_info()
 
       choices <- setNames(issues$number, paste0("Issue ", issues$number, ": ", issues$title))
 
@@ -24,32 +24,24 @@ ghqc_update_server <- function(id) {
     })
 
     observeEvent(input$preview, {
+      req(input$select_issue)
+      html_file_path <- create_gfm_file(create_comment_body(get_organization(),
+                                                            get_current_repo(),
+                                                            issue_number = input$select_issue,
+                                                            diff = input$show_diff, #TODO: check outputs
+                                                            force = TRUE))
+      custom_html <- readLines(html_file_path, warn = FALSE) %>% paste(collapse = "\n")
 
-      html_file_path <- "example.html"
-        custom_html <- readLines(html_file_path, warn = FALSE) %>% paste(collapse = "\n")
-
-      #---
-
-      #create_comment_body(owner = get_organization(), repo = get_current_repo(), issue_number = 1, message = "TEST")
-      # Error in gert::git_branch_create(temp_branch, ref = commit_sha) :
-      # Failed to find git reference 'd9e7eff4746ac0768beea63957dd09b54b0a482e'
-
-      # Error in if (stringr::str_detect(line, "^\\*")) { :
-      #     missing value where TRUE/FALSE needed
-      #---
-     # html_file_path <- "example.html"
-     # custom_html <- readLines(html_file_path, warn = FALSE) %>% paste(collapse = "\n")
-
-      modal_content <- case_when(
-          !input$show_diff ~ custom_html,
-          input$show_diff && input$compare == "init" ~ '<h1>Initial QC Request</h1><p>Comparing to the initial quality control request.</p>',
-          input$show_diff && input$compare == "prev" ~ '<h1>Previous QC Update</h1><p>Comparing to the previous quality control update.</p>',
-          TRUE ~ '<h1>Unexpected Option</h1><p>An unexpected option was selected.</p>'
-        )
+      # modal_content <- case_when(
+      #     !input$show_diff ~ custom_html,
+      #     input$show_diff && input$compare == "init" ~ '<h1>Initial QC Request</h1><p>Comparing to the initial quality control request.</p>',
+      #     input$show_diff && input$compare == "prev" ~ '<h1>Previous QC Update</h1><p>Comparing to the previous quality control update.</p>',
+      #     TRUE ~ '<h1>Unexpected Option</h1><p>An unexpected option was selected.</p>'
+      #   )
 
       showModal(modalDialog(
         title = "Comment Preview",
-        renderUI(HTML(modal_content)),
+        renderUI(HTML(custom_html)),
         footer = modalButton("Close")
       ))
     })

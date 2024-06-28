c5bd464da9fd637fba9773c80957f7cd3be5b0b6
Anne Zheng <anne@a2-ai.com>
2024-06-28 15:25:30 +0000

    fix import also remove some todo's

--- R/ghqc_update_server.R
+++ R/ghqc_update_server.R
@@ -1,6 +1,6 @@
 #' @import shiny
 #' @importFrom shinyjs enable disable addClass removeClass
-#' @importFrom dplyr case_when
+#' @import dplyr
 NULL
 
 ghqc_update_server <- function(id) {
@@ -13,18 +13,6 @@ ghqc_update_server <- function(id) {
 
     observe({
 
-      # nest issues under milestone name group. need to set single elements as list or else won't nest https://stackoverflow.com/questions/57248427/grouped-select-input-with-only-one-item
-      # TODO: need to finish re-sort list after split so that most recent milestones are on top rather than alphabetical
-      # sorted_issues <- issues %>%
-      #   dplyr::mutate(milestone_order = as.numeric(factor(milestone_created, levels = unique(milestone_created)))) %>%
-      #   split(.$milestone) %>%
-      #   lapply(function(x) {
-      #     setNames(object = as.list(paste(x$number, x$title)),
-      #              nm = paste0("Item ", x$number, ": ", x$title))
-      # })
-
-      # req(issues())
-      # milestone_list <- unique(issues()$milestone)
       milestone_list <- get_open_milestones(org = get_organization(), repo = get_current_repo())
       milestone_list <- rev(milestone_list)
 
@@ -61,28 +49,6 @@ ghqc_update_server <- function(id) {
         server = TRUE
       )
     })
-    #checks:
-    # git sync status - same as before
-    # file status - pull issue title name and compare to see if in git panel #TODO: check if this works
-    # also keep the git file check
-    #TODO: ask jenna to have functions to check for 1. if there are no commits or comments to return false instead of error
-    # > check_if_there_are_update_comments(owner = get_organization(), repo = get_current_repo(), issue_number = 3) # this has no comments
-    # Error in UseMethod("arrange") :
-    #   no applicable method for 'arrange' applied to an object of class "NULL"
-    # > check_if_there_are_update_comments(owner = get_organization(), repo = get_current_repo(), issue_number = 5)
-    # [1] TRUE
-    # Only way this button should proceed is if
-    # 1. there are no unstaged changes in local of the file
-    # 2. git status is current
-    # 3. There is a new commit different from init commit sha
-    #  preview and post should have same proceed checks
-
-    # init/prev differences should be the same if there is no prev update comment. init should look at first comment for sha;
-    # prev should loop from latest comment until it founds most recent sha
-
-    # workflow should be (?) 1. create qc item list thru create qc app 2. assignee/qcer comments issues on gh 3. author fixes issues and pushes new commit
-    # 4. author uses update qc app to add fix comment on prev commit 5-7. repeat 2-4 as needed 8. after author addresses everything, uses init compare for final comment
-    # 9. author repeats for all qc items in list 10. author scrapes the milestone/qc item list for archive items thru a fxn
 
     issue_parts <- reactive({
       req(input$select_issue)

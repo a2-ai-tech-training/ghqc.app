2b63b8a27955728957d7d8aa51785e01bbc5185d
jenna-a2ai <jenna@a2-ai.com>
2024-07-01 16:45:47 +0000

    background jobs

--- R/run_apps_as_bgj.R
+++ R/run_apps_as_bgj.R
@@ -1,7 +1,7 @@
-run_app_in_background <- function() {
+run_app_in_background <- function(script) {
   port <- httpuv::randomPort()
   Sys.setenv("GHQC_SHINY_PORT" = port)
-  job_id <- rstudioapi::jobRunScript(temp_file)
+  job_id <- rstudioapi::jobRunScript(script)
   Sys.sleep(5)
   rstudioapi::viewer(sprintf("https://127.0.0.1:%s", port))
 }

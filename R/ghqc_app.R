5352fabab3f7140ad7b8627571b0b16e1fc6f0c1
jenna-a2ai <jenna@a2-ai.com>
2024-07-01 14:00:38 +0000

    checklists and no diffs

--- R/ghqc_app.R
+++ R/ghqc_app.R
@@ -14,3 +14,33 @@ ghqc_app <- function() {
     viewer = paneViewer()
   )
 }
+
+# ghqc_app <- function() {
+#   ui <- ghqc_ui(
+#     id = "ghqc_app")
+#
+#   server <- function(input, output, session) {
+#     ghqc_server(
+#       id = "ghqc_app"
+#     )
+#   }
+#   shiny::runApp(list(ui = ui, server = server), port = as.numeric(Sys.getenv("GHQC_SHINY_PORT", 5454)))
+# }
+
+# ghqc_app <- function() {
+#   #.libPaths("~/ghqc-rpkgs/")
+#
+#   runApp(list(
+#     ui = ghqc_ui(
+#       id = "ghqc_app"),
+#     server = function(input, output, session) {
+#       ghqc_server(
+#         id = "ghqc_app"
+#       )
+#     },
+#     #viewer = rstudioapi::viewer(sprintf("https://127.0.0.1:%s", port)),
+#     port = as.numeric(Sys.getenv("GHQC_SHINY_PORT", 5454))
+#     #shiny::runApp(list(ui = ui, server = server), port = as.numeric(Sys.getenv("GHQC_SHINY_PORT", 5454)))
+#   )
+#   )
+# }
--- R/qc_fix_helper.R
+++ R/qc_fix_helper.R
@@ -123,7 +123,8 @@ format_diff <- function(file_path, commit_sha_orig, commit_sha_new) {
   # get the line indices with the file names (either 1,2 or 2,3 depending on if the the files were the same)
   file_index_start <- {
     if (diff_lines[1] == "No visible differences between objects.") {
-      2
+      #2
+      return("No difference between file versions.")
     }
     else {
       1
@@ -147,8 +148,6 @@ format_diff <- function(file_path, commit_sha_orig, commit_sha_new) {
     diff_lines <- diff_lines[-c(length(diff_lines))]
   }
 
-
-
   format_diff_for_github <- function(diff_lines) {
     result <- c()
     for (line in diff_lines) {
@@ -166,7 +165,9 @@ format_diff <- function(file_path, commit_sha_orig, commit_sha_new) {
   github_diff <- format_diff_for_github(diff_lines)
 
   diff_cat <- glue::glue_collapse(github_diff, sep = "\n")
+
   diff_with_line_numbers <- add_line_numbers(diff_cat)
+
   glue::glue("```diff\n{diff_with_line_numbers}\n```")
 }
 

59d08755d569dca4b44bf91597fbd55e6260cfb6
jenna-a2ai <jenna@a2-ai.com>
2024-07-19 15:58:24 +0000

    getting back lost changes from rebase

--- R/format.R
+++ R/format.R
@@ -1,3 +1,4 @@
+
 format_issue_body <- function(checklist_type, file_path) {
   checklists <- get_checklists()
   file_items <- checklists[[checklist_type]]
@@ -41,23 +42,85 @@ get_sha <- function() {
   commits$commit[1]
 }
 
-get_git_user <- function() {
-  git_user_info <- gert::git_config_global()
-  user_name <- git_user_info[git_user_info$name == "user.name", "value"]
+
+get_authors <- function(file_path) {
+  # https://stackoverflow.com/questions/11533199/how-to-find-the-commit-in-which-a-given-file-was-added
+
+  # shell out
+  # git log --follow -- file_path
+  log <- processx::run("git", c("log", "--follow", "--", file_path))$stdout
+  # get lines with author
+  author_lines <- unlist(stringr::str_extract_all(log, "Author:.*"))
+  # remove "Author: " prefix
+  authors <- stringr::str_remove(author_lines, "Author: ")
+  # get most recent author
+  latest_author <- authors[1]
+  # get unique authors
+  unique_authors <- unique(stringr::str_remove(authors, "Author: "))
+  # get collaborators besides most recent author
+  other_collaborators <- unique_authors[unique_authors != latest_author]
+
+  list(latest = latest_author,
+       collaborators = other_collaborators)
+}
+
+format_collaborators <- function(collaborators, prefix = "") {
+  if (length(collaborators) > 0) {
+    collaborators_cat <- glue::glue_collapse(collaborators, ", ")
+    glue::glue("{prefix}{collaborators_cat}")
+  }
+  else {
+    ""
+  }
 }
 
 format_metadata <- function(checklist_type, file_path) {
-  author <- get_git_user()
+  authors <- get_authors(file_path)
+  latest_author <- authors$latest
+  author_section <- glue::glue("* author: {latest_author}")
+  metadata <- c(author_section)
+
+  collaborators_section <- format_collaborators(authors$collaborators, prefix = "* collaborators: ")
+  if (collaborators_section != "") {
+    metadata <- c(metadata, collaborators_section)
+  }
+
   qc_type <- checklist_type
+  qc_type_section <- glue::glue("* qc type: {qc_type}")
+
   script_hash <- digest::digest(file = file_path)
+  script_hash_section <- glue::glue("* script hash: {script_hash}")
+
   git_sha <- get_sha()
-  glue::glue("
-             * author: {author}
-             * qc type: {qc_type}
-             * script hash: {script_hash}
-             * git sha: {git_sha}")
+  git_sha_section <- glue::glue("* git sha: {git_sha}")
+
+  file_history_url <- get_file_history_url(file_path)
+  file_history_url_section <- glue::glue("* file history: {file_history_url}")
+
+  metadata <- c(metadata, qc_type_section, script_hash_section, git_sha_section, file_history_url_section)
+
+  glue::glue_collapse(metadata, "\n")
 }
 
+get_file_history_url <- function(file_path) {
+  # get branch
+  branch <- gert::git_branch()
 
+  # get remote url (assume first row)
+  remote_url <- gert::git_remote_list()$url[1]
 
+  # if it's an ssh, construct manually
+  if (grepl("^git@", remote_url)) {
+    # get the domain and repo
+    domain <- sub("git@(.*):.*", "\\1", remote_url)
+    repo_path <- sub("git@.*:(.*)", "\\1", remote_url)
 
+    remote_url <- glue::glue("https://{domain}/{repo_path}")
+  }
+
+  # take out .git at the end
+  https_url <- sub(".git$", "", remote_url)
+
+  # get something like https://github.com/A2-ai/project_x/commits/main/scripts/DA.R
+  file_history_url <- glue::glue("{https_url}/commits/{branch}/{file_path}")
+}

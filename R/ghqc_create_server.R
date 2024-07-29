9298afe2311996e0d6ac83b36b54c482df9cae8b
Anne Zheng <anne@a2-ai.com>
2024-07-29 13:30:14 +0000

    replace waiter hide delay with msg from after adjust

--- R/ghqc_create_server.R
+++ R/ghqc_create_server.R
@@ -25,6 +25,7 @@ ghqc_create_server <- function(id) {
 
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
+
     qc_trigger <- reactiveVal(FALSE)
 
     waiter_hide()
@@ -126,20 +127,22 @@ return "<div><strong>" + escape(item.username) + "</div>"
     output$main_panel <- renderUI({
       validate(need(length(selected_items()) > 0, "No files selected"))
       w_load_items$show()
+
       log_string <- glue::glue_collapse(selected_items(), sep = ", ")
       info(.le$logger, glue::glue("Files selected for QC: {log_string}"))
+
       list <- render_selected_list(input, ns, items = selected_items(), checklist_choices = get_checklists())
       isolate_rendered_list(input, session, selected_items())
 
-      session$sendCustomMessage("adjust_grid", list) # finds the width of the files and adjusts grid column spacing based on values
+      session$sendCustomMessage("adjust_grid", id) # finds the width of the files and adjusts grid column spacing based on values
       return(list)
     })
 
-    observeEvent(c(selected_items(), input$assignees), {
-      delay(300, {
-        w_load_items$hide()
-      })
-    }, ignoreInit = TRUE)
+    observe({
+      req(input$adjust_grid_finished) # retrieve msg through js when adjust grid is done
+      w_load_items$hide()
+    })
+
     # button behavior
     observe({
       debug(.le$logger, glue::glue("create_qc_items buttons are inactivated."))

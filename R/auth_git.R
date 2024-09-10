4e5fcefe5ab9c0a06ab446b079d3efa0374f782c
jenna-a2ai <jenna@a2-ai.com>
2024-09-10 20:40:58 +0000

    fixed bug

--- R/auth_git.R
+++ R/auth_git.R
@@ -20,6 +20,8 @@ check_remote_set <- function() {
 }
 
 check_upstream_set <- function(remote_name) {
+  repo <- get_simple_path()
+
   current_branch <- gert::git_branch()
 
   if (is.null(current_branch)){

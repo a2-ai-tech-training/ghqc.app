a11fb4794f8f9e438c0517e8ff2045260985305c
Wes Cummings <wes@a2-ai.com>
2024-10-14 17:59:32 +0000

    resolving assign app conflict

--- R/app_01_assign.R
+++ R/app_01_assign.R
@@ -2,12 +2,8 @@
 NULL
 
 #' @export
-<<<<<<< HEAD:R/app_01_create.R
-ghqc_create_app <- function() {
-  if (!exists("info_repo_path", .le)) ghqc_set_info_repo()
-=======
 ghqc_assign_app <- function() {
->>>>>>> main:R/app_01_assign.R
+  if (!exists("info_repo_path", .le)) ghqc_set_info_repo()
   app <- shinyApp(
     ui = ghqc_assign_ui(
       id = "ghqc_assign_app"
